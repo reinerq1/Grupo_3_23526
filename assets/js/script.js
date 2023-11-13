c4c6bfa44dd5e0d55d929741d742dba3fa2d6e75
GabrielBMorello9 <morellogabriel24@gmail.com>
2023-11-13 19:29:14 -0300

    menu responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const botoMenu = document.querySelector("#menu_responsive");
   const navegador = document.querySelector("#div__navegador");
   const botonCerrarMenu = document.querySelector("#cerrar_menu_responsive");
-  console.log(navegador.classList);
+ 
   botoMenu.addEventListener("click", () => {
     navegador.classList.add("block");
     botoMenu.classList.add("none");
